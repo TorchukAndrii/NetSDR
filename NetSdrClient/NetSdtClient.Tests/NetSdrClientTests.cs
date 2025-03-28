e993bd10205cb1e1f0746ca3a1792a966226e065
Andrii.Torchuk <Andrii.Torchuk2@gmail.com>
2025-03-28 12:47:20 +0200

    Unit tests for NetSdrClient

--- NetSdrClient/NetSdtClient.Tests/NetSdrClientTests.cs
+++ NetSdrClient/NetSdtClient.Tests/NetSdrClientTests.cs
@@ -1,9 +1,32 @@
+using System.Net;
+using Moq;
+using NetSdrClient.Contracts;
+using NetSdrClient.Exceptions;
+
 namespace NetSdtClient.Tests;
 
 public class NetSdrClientTests
 {
     [Fact]
-    public void Test1()
+    public async Task ConnectAsync_ValidIp_EstablishesConnection()
     {
+        var tcpClientMock = new Mock<ITcpCommunicationClient>();
+        var udpReceiverMock = new Mock<IUdpReceiver>();
+        var client = new NetSdrClient.NetSdrClient(tcpClientMock.Object, udpReceiverMock.Object);
+
+        await client.ConnectAsync("127.0.0.1", 50000);
+        tcpClientMock.Verify(c => c.ConnectAsync(It.IsAny<IPAddress>(), 50000), Times.Once);
+    }
+
+    [Fact]
+    public async Task DisconnectAsync_CallsDisconnectOnClients()
+    {
+        var tcpClientMock = new Mock<ITcpCommunicationClient>();
+        var udpReceiverMock = new Mock<IUdpReceiver>();
+        var client = new NetSdrClient.NetSdrClient(tcpClientMock.Object, udpReceiverMock.Object);
+
+        await client.DisconnectAsync();
+        tcpClientMock.Verify(c => c.DisconnectAsync(), Times.Once);
+        udpReceiverMock.Verify(c => c.StopReceivingAsync(), Times.Once);
     }
 }
