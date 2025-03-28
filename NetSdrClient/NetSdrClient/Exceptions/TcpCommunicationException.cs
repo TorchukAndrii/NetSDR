1a8e4ae5b37a783ee29737f785e7e692fd1c7e2a
Andrii.Torchuk <Andrii.Torchuk2@gmail.com>
2025-03-28 08:48:54 +0200

    Add exceptions

---
+++ NetSdrClient/NetSdrClient/Exceptions/TcpCommunicationException.cs
@@ -0,0 +1,12 @@
+﻿namespace NetSdrClient.Exceptions;
+
+public class TcpCommunicationException : Exception
+{
+    public TcpCommunicationException(string message, Exception ex) : base(message, ex)
+    {
+    }
+
+    public TcpCommunicationException(string message) : base(message)
+    {
+    }
+}
